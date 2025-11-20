45aa1f3672d9134197319e9603d9b53d7c670794
Валерия Зайкина <vchxve@mail.ru>
2025-11-20 06:03:53 +0500

    Add complete combat system: player movement, ultimate system with UI, AoE damage, loot system

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,20 +2,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 5f;
+    private Vector2 movement;
+
+    [Header("Combat")]
+    public UltimateSystem ultimateSystem;
+    public int clickDamage = 1;
+
+    [Header("Health")]
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+
+        if (ultimateSystem == null)
+            ultimateSystem = GetComponent<UltimateSystem>();
+    }
 
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        // Обработка движения
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = new Vector3(moveX, moveY, 0);
-
-        movement = movement.normalized;
-
-        transform.position += movement * moveSpeed * Time.deltaTime;
-
+        // Обработка кликов (отдельно от движения)
         CheckForEnemyClick();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Движение в FixedUpdate для плавности
+        if (rb != null)
+        {
+            rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            // Резервный вариант если нет Rigidbody
+            transform.position += (Vector3)movement.normalized * moveSpeed * Time.deltaTime;
+        }
     }
 
     void CheckForEnemyClick()
@@ -30,9 +66,26 @@ public class PlayerController : MonoBehaviour
                 Enemy enemy = hit.collider.GetComponent<Enemy>();
                 if (enemy != null)
                 {
-                    enemy.TakeDamage(1);
+                    // Наносим урон врагу
+                    enemy.TakeDamage(clickDamage);
+
+                    // Заряжаем ультимейт
+                    if (ultimateSystem != null)
+                        ultimateSystem.AddCharge(1);
                 }
             }
         }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Игрок получил урон! Здоровье: " + currentHealth);
+    }
+
+    void Die()
+    {
+        Debug.Log("Игрок умер! Конец забега");
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/_Scripts/UltimateSystem.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UltimateSystem : MonoBehaviour
+{
+    [Header("Ultimate UI")]
+    public Image ultimateFill;
+    public TextMeshProUGUI ultimateText;
+
+    [Header("Settings")]
+    public int currentCharge = 0;
+    public int chargeNeeded = 10;
+    public float ultimateDamage = 50f;
+    public float ultimateRadius = 3f;
+
+    private float maxWidth = 300f; // Совпадает с Width Background
+
+    void Update()
+    {
+        UpdateUI();
+
+        if (currentCharge >= chargeNeeded)
+        {
+            ActivateUltimate();
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (ultimateFill != null && ultimateText != null)
+        {
+            // Вычисляем проценты
+            float fillPercent = (float)currentCharge / chargeNeeded;
+            int percent = Mathf.RoundToInt(fillPercent * 100);
+
+            // Меняем ширину заполнения
+            float newWidth = maxWidth * fillPercent;
+            RectTransform fillRect = ultimateFill.GetComponent<RectTransform>();
+            fillRect.sizeDelta = new Vector2(newWidth, fillRect.sizeDelta.y);
+
+            // Обновляем текст
+            ultimateText.text = percent + "%";
+        }
+    }
+
+    public void AddCharge(int amount)
+    {
+        currentCharge = Mathf.Min(currentCharge + amount, chargeNeeded);
+    }
+
+    void ActivateUltimate()
+    {
+        Debug.Log("УЛЬТИМЕЙТ АКТИВИРОВАН!");
+
+        // AoE урон по врагам в радиусе
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, ultimateRadius);
+        foreach (Collider2D enemyCollider in hitEnemies)
+        {
+            if (enemyCollider.CompareTag("Enemy"))
+            {
+                Enemy enemy = enemyCollider.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.TakeDamage((int)ultimateDamage);
+                    Debug.Log("AoE урон по врагу: " + ultimateDamage);
+                }
+            }
+        }
+
+        // Сбрасываем и увеличиваем сложность
+        currentCharge = 0;
+        chargeNeeded += 5;
+        ultimateDamage += 10f;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ultimateRadius);
+    }
+}
